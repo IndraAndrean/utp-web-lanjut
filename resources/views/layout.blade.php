c5813beffcab7bb3af87eaf1658c0eb395916d1e
Indra Andrean <143471956+IndraAndrean@users.noreply.github.com>
2025-05-10 12:06:36 +0700

    Part 7

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -92,6 +92,7 @@
             <a href="{{ url('/students') }}">Student</a>
             <a href="{{ url('/teachers') }}">Teacher</a>
             <a href="{{ url('/courses') }}">Course</a>
+            <a href="{{ url('/batches') }}">Batches</a>
             <a href="#about">Enrollment</a>
             <a href="#about">Payment</a>
             </div>

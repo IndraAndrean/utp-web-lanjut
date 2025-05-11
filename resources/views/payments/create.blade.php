2bfcda998a4f81270321aa89e45933a095dcd679
Indra Andrean <143471956+IndraAndrean@users.noreply.github.com>
2025-05-11 20:25:05 +0700

    Part 12

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -2,24 +2,25 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Batches</div>
+  <div class="card-header">Payments</div>
   <div class="card-body">
       
-      <form action="{{ url('batches') }}" method="post">
+      <form action="{{ url('payments') }}" method="post">
         {!! csrf_field() !!}
-        <label>Batch Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
-
-        <label>Course</label></br>
-        <!-- <input type="text" name="course_id" id="course_id" class="form-control"></br> -->
-         <select name="course_id" id="course_id" class="form-control"> 
-          @foreach($courses as $id => $name)
-          <option value="{{ $id }}">{{ $name }}</option>
+        <label>Enrollment No</label></br>
+        <select name="enrollment_id" id="enrollment_id" class="form-control"> 
+          @foreach($enrollments as $id => $enroll_no)
+          <option value="{{ $id }}">{{ $enroll_no }}</option>
           @endforeach
          </select>
 
-        <label>Start Date</label></br>
-        <input type="text" name="start_date" id="start_date" class="form-control"></br>
+
+        <label>Paid Date</label></br>
+        <input type="text" name="paid_date" id="paid_date" class="form-control"></br>
+         
+
+        <label>Amount</label></br>
+        <input type="text" name="amount" id="amount" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
@@ -27,3 +28,4 @@
 </div>
  
 @stop
+
